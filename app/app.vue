c0baa597830e415569b3f34f563b3e9a2927bf38
mijacke <lassumario2002@gmail.com>
2025-12-11 00:29:21 +0100

    chore: update config (tailwind, nuxt, packages) and add burger menu animation CSS

--- app/app.vue
+++ app/app.vue
@@ -1,31 +1,5 @@
-<script setup>
-// Critical styles applied immediately to prevent FOUC
-useHead({
-  style: [
-    {
-      innerHTML: `
-        html { 
-          font-family: 'Quicksand', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif;
-          scroll-behavior: smooth;
-        }
-        body { 
-          margin: 0;
-          padding: 0;
-          background-color: #f5f3ec;
-          color: #1f1f1a;
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-        }
-      `
-    }
-  ]
-})
-</script>
-
 <template>
-  <div>
-    <NuxtLoadingIndicator color="#535943" />
-    <NuxtRouteAnnouncer />
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
