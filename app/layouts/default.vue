1cd302054acac32184e54f19295e8e4b0aaf975c
mijacke <lassumario2002@gmail.com>
2025-12-11 12:20:00 +0100

    feat: add default layout with InstagramSection and Splide types

---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <LayoutTheHeader />
+    <main class="flex-grow pt-20 md:pt-24">
+      <slot />
+    </main>
+    <SectionsHomeInstagramSection />
+    <LayoutTheFooter />
+  </div>
+</template>
